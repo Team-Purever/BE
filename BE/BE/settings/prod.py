f79984918220f6f7a8f5e8e39dc20ecd030f4a57
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-31 13:31:04 +0900

    cors 추가

--- BE/BE/settings/prod.py
+++ BE/BE/settings/prod.py
@@ -1,3 +1,11 @@
 from .base import *
 
+CORS_ORIGIN_ALLOW_ALL = False 
+
+# or 
+
+CORS_ORIGIN_WHITELIST = (
+    "https://ec2-3-24-194-74.ap-southeast-2.compute.amazonaws.com",
+)
+
 ALLOWED_HOSTS = ['.ap-northeast-2.compute.amazonaws.com']
